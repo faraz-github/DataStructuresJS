6bfd8d3ec2122aa919377811930a82cdfc8e9ac0
faraz-github <farazahmad.email@gmail.com>
2023-07-08 23:47:21 +0530

    Stack - part 3

--- customStructures.js
+++ customStructures.js
@@ -18,3 +18,47 @@
 // d) isEmpty
 // e) size
 // f) print
+
+// Stack implementation
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+// Create new stack instance
+const stack = new Stack();
+
+// Testing
+console.log(stack.isEmpty());
+stack.push(20);
+stack.push(10);
+stack.push(30);
+console.log(stack.size());
+stack.print();
+console.log(stack.pop());
+console.log(stack.peek());
