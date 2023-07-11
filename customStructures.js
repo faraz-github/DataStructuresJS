3f63fc6d0f112c6c0ebb5d0a38d432d54ea58fde
faraz-github <farazahmad.email@gmail.com>
2023-07-11 23:24:55 +0530

    Optimised queue

--- customStructures.js
+++ customStructures.js
@@ -63,7 +63,7 @@ stack.print();
 console.log(stack.pop());
 console.log(stack.peek());
 
-// 1. Queue
+// 2. Queue
 // ======================================================================================================================================
 
 // a) queue is a sequential collection of elements
@@ -130,3 +130,56 @@ console.log(queue.size());
 queue.print();
 console.log(queue.dequeue());
 console.log(queue.peek());
+
+// 2. a) optimisedQueue
+// ======================================================================================================================================
+
+// Note: An optimised queue used an object instead of the array which makes its Big-O time complexity constant instead of linear.
+
+class OptimisedQueue {
+  constructor() {
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const optimisedQueue = new OptimisedQueue();
+
+console.log(optimisedQueue.isEmpty());
+optimisedQueue.enqueue("Apple");
+optimisedQueue.enqueue("Banana");
+optimisedQueue.enqueue("Guava");
+console.log(optimisedQueue.size());
+optimisedQueue.print();
+console.log(optimisedQueue.dequeue());
+console.log(optimisedQueue.dequeue());
+console.log(optimisedQueue.peek());
