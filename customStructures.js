dbc2075976175554dde93810256669d02444d9a3
faraz-github <farazahmad.email@gmail.com>
2023-07-13 23:55:24 +0530

    Circular queue - part 2

--- customStructures.js
+++ customStructures.js
@@ -199,3 +199,56 @@ console.log(optimisedQueue.peek());
 //      1) clock
 //      2) streaming data
 //      3) traffic lights
+
+// Circular Queue operations
+// a) Enqueue
+// b) Dequeue - remove the oldest element from the queue
+// c) isFull
+// d) isEmpty
+// e) peek
+// f) size
+// g) print
+
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.rear = -1;
+    this.front = -1;
+  }
+
+  isFull() {
+    return this.currentLength === this.capacity;
+  }
+
+  isEmpty() {
+    return this.currentLength === 0;
+  }
+
+  enqueue(element) {
+    if (!this.isFull) {
+      this.rear = (this.rear + 1) % this.capacity;
+      this.items[this.rear] = element;
+      this.currentLength += 1;
+      if (this.front === -1) {
+        this.front = this.rear;
+      }
+    }
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const item = this.items[this.front];
+    this.items[this.front] = null;
+    this.front = (this.front + 1) % this.capacity;
+    this.currentLength -= 1;
+    if (this.isEmpty()) {
+      this.front = -1;
+      this.rear = -1;
+    }
+    return item;
+  }
+}
