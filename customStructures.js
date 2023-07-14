c335ffac6ae2e426611b2a0cd2e17d4cb2d39a95
faraz-github <farazahmad.email@gmail.com>
2023-07-14 23:58:55 +0530

    Circular queue - part 3

--- customStructures.js
+++ customStructures.js
@@ -206,16 +206,15 @@ console.log(optimisedQueue.peek());
 // c) isFull
 // d) isEmpty
 // e) peek
-// f) size
 // g) print
 
 class CircularQueue {
   constructor(capacity) {
     this.items = new Array(capacity);
-    this.capacity = capacity;
-    this.currentLength = 0;
     this.rear = -1;
     this.front = -1;
+    this.currentLength = 0;
+    this.capacity = capacity;
   }
 
   isFull() {
@@ -226,10 +225,14 @@ class CircularQueue {
     return this.currentLength === 0;
   }
 
-  enqueue(element) {
-    if (!this.isFull) {
+  size() {
+    return this.currentLength;
+  }
+
+  enqueue(item) {
+    if (!this.isFull()) {
       this.rear = (this.rear + 1) % this.capacity;
-      this.items[this.rear] = element;
+      this.items[this.rear] = item;
       this.currentLength += 1;
       if (this.front === -1) {
         this.front = this.rear;
@@ -251,4 +254,41 @@ class CircularQueue {
     }
     return item;
   }
+
+  peek() {
+    if (!this.isEmpty()) {
+      return this.items[this.front];
+    }
+    return null;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("Queue is empty");
+    } else {
+      let i;
+      let str = "";
+      for (i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
+        str += this.items[i] + " ";
+      }
+      str += this.items[i];
+      console.log(str);
+    }
+  }
 }
+
+const circularQueue = new CircularQueue(5);
+console.log(circularQueue.isEmpty());
+circularQueue.enqueue(10);
+circularQueue.enqueue(20);
+circularQueue.enqueue(30);
+circularQueue.enqueue(40);
+circularQueue.enqueue(50);
+console.log(circularQueue.size());
+circularQueue.print();
+console.log(circularQueue.isFull());
+console.log(circularQueue.dequeue());
+console.log(circularQueue.peek());
+circularQueue.print();
+circularQueue.enqueue(60);
+circularQueue.print();
