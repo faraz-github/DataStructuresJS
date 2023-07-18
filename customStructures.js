f214defd00bc0cc728d6591377c45b5f13ca8e2a
faraz-github <farazahmad.email@gmail.com>
2023-07-18 23:26:04 +0530

    Linked list - part 4

--- customStructures.js
+++ customStructures.js
@@ -341,6 +341,22 @@ class LinkedList {
 
     this.size++;
   }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+
+      console.log(listValues);
+    }
+  }
 }
 
 const linkedList = new LinkedList();
@@ -348,6 +364,11 @@ const linkedList = new LinkedList();
 console.log("List is empty?", linkedList.isEmpty());
 console.log("List size", linkedList.getSize());
 
+linkedList.print();
+
 linkedList.prepend(10);
+linkedList.print();
 linkedList.prepend(20);
 linkedList.prepend(30);
+
+linkedList.print();
