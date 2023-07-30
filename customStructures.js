a70cd0e6b0114dd4382464f24ccd97480c859430
faraz-github <farazahmad.email@gmail.com>
2023-07-30 23:25:48 +0530

    Doubly linked list

--- customStructures.js
+++ customStructures.js
@@ -729,3 +729,11 @@ console.log(linkedListQueue.getSize());
 linkedListQueue.print();
 console.log(linkedListQueue.dequeue());
 console.log(linkedListQueue.peek());
+
+// LinkedList has one more variation called DoublyLinkedList
+
+// Doubly Linked List is a variation of Linked list in which navigation is possible in both ways, either forward and backward easily as compared to Single Linked List.
+// Link − Each link of a linked list can store a data called an element.
+// Next − Each link of a linked list contains a link to the next link called Next.
+// Prev − Each link of a linked list contains a link to the previous link called Prev.
+// Linked List − A Linked List contains the connection link to the first link called HEAD and to the last link called TAIL.
