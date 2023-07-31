a15b78fcd511d5a82ad03aaec9a352876793506c
faraz-github <farazahmad.email@gmail.com>
2023-07-31 23:10:18 +0530

    Hash Table - part 1

---
+++ hashTable.js
@@ -0,0 +1,20 @@
+// Hash Table
+// A hash table, also known as hash map, is a data structure that is used to store key-value pairs.
+// It is generally used for fast lookup to the value compared to other similar structures.
+
+// A hashing function accepts the string key, converts it into a hash code using a defined logic and then
+// and then maps it into numeric index that is within the bounds of an array.
+
+// Using the index, store the value
+// The same hashing function is reused to retrieve the value given a key.
+
+// Main operations of Hash Table
+// a) Set
+// b) Get
+// c) Remove
+
+// Main Applications
+// a) Database indexing
+// b) Caches
+
+// Side note: map is better than objects
