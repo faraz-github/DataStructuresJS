902e9c3b1101ab79d2255eabffc6505bd6838aed
faraz-github <farazahmad.email@gmail.com>
2023-08-03 23:43:46 +0530

    Tree - part 1

---
+++ tree.js
@@ -0,0 +1,26 @@
+// Tree
+// A tree is a hierarchical data structure that consist of nodes connected by edges
+// A tree is a non-linear data structure, compared to arrays, linked lists, stacks and queues which are linear data structure.
+// In linear data structures, the time required to search is propotional to the size of the data set.
+// Trees however, owing to the non linear nature allow quicker and easier access to the data.
+// A tree will not contain any loops or cycles.
+
+// Tree Applications
+// a) File systems for directory structure
+// b) A family tree
+// c) An organisation tree
+// d) Document object model DOM
+// e) Chat bots
+// f) Abstract syntax trees
+
+// Terminologies used in Tree Data Structure
+// a) Root Node
+// b) Parent Nodes
+// c) Child Nodes
+// d) Sibling Nodes
+// e) Leaf Nodes
+// f) Ancestor
+// g) Path
+// h) Degree
+// i) Depth
+// j) Height
