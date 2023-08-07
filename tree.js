c2bd4bde69ef669b2110f34717c7ea3c0e6892f0
faraz-github <farazahmad.email@gmail.com>
2023-08-07 23:18:30 +0530

    Tree - part 3

--- tree.js
+++ tree.js
@@ -40,3 +40,24 @@
 // a) Searching
 // b) Sorting
 // c) Implement abstract data types such as look up tables and priority queue
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root == null;
+  }
+}
+
+const bst = new BinarySearchTree();
+console.log("Tree is empty?", bst.isEmpty());
