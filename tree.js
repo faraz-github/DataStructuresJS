054856191d24b77990a0cf14ed07538e9ba1af13
faraz-github <farazahmad.email@gmail.com>
2023-08-09 23:40:49 +0530

    Tree - part 5

--- tree.js
+++ tree.js
@@ -82,6 +82,20 @@ class BinarySearchTree {
       }
     }
   }
+
+  search(root, value) {
+    if (!root) {
+      return false;
+    } else {
+      if (root.value === value) {
+        return true;
+      } else if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
@@ -90,3 +104,8 @@ console.log("Tree is empty?", bst.isEmpty());
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
+
+console.log(bst.search(bst.root, 10));
+console.log(bst.search(bst.root, 5));
+console.log(bst.search(bst.root, 15));
+console.log(bst.search(bst.root, 20));
