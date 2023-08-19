0bab8bfebbe3189b703b970c287bdac297752ff2
faraz-github <farazahmad.email@gmail.com>
2023-08-19 23:55:02 +0530

    Graph - part 1

---
+++ graph.js
@@ -0,0 +1,16 @@
+// Graph
+// graph is a non-linear data structure that consists of a finite number of vertices(nodes) connected by edges.
+// trees are specific type of graph data structure
+// It can be of two types
+// - Directed (Edges have a one direction for traversal)
+// - Undirected (Edges are bidirectional to traverse)
+// They may contain
+// - Only nodes
+// - Edges from a node
+// - Cycles
+// - Self loop
+// - Disconnected nodes
+// - It may contain waits on the edges (Cost of traversal)
+// EXAMPLES
+// - Google Maps
+// - Social Media Sites
