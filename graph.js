9aafb2be48e89e6aa884021513911308f191ba3d
faraz-github <farazahmad.email@gmail.com>
2023-08-22 23:33:45 +0530

    Graph - part 4

--- graph.js
+++ graph.js
@@ -31,3 +31,35 @@ const adjacencyList = {
 };
 
 console.log(adjacencyList["C"]);
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = new Set();
+    }
+  }
+
+  addEdge(vertexOne, vertexTwo) {
+    if (!this.adjacencyList[vertexOne]) {
+      this.addVertex(vertexOne);
+    }
+    if (!this.adjacencyList[vertexTwo]) {
+      this.addVertex(vertexTwo);
+    }
+
+    this.adjacencyList[vertexOne].add(vertexTwo); // add is available on Set
+    this.adjacencyList[vertexTwo].add(vertexOne);
+  }
+}
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
